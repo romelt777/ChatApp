6b622353638b59408d25674f7301b7d3e0af65bf
romelt777 <romel.tan777@gmail.com>
2025-04-24 16:28:52 -0300

    changed naming

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/Screens/HomeScreen.dart';
+import 'package:chat_app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -26,7 +26,7 @@ class MainApp extends StatelessWidget {
           secondary: const Color(0xFF128C7E),
         ),
       ),
-      home: Homescreen(),
+      home: HomeScreen(),
   
     );
   }
