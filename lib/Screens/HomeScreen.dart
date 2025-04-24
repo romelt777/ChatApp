954d47944ea1fcebe3b2b6a6d65f94cfe41941d6
romelt777 <romel.tan777@gmail.com>
2025-04-24 16:04:45 -0300

    finished tab bar

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,46 +1,49 @@
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatefulWidget {
-  const Homescreen({super.key});
+    const Homescreen({super.key});
 
-  @override
-  State<Homescreen> createState() => _HomescreenState();
+    @override
+    State<Homescreen> createState() => _HomescreenState();
 }
 
+//needs ticker for animations
 class _HomescreenState extends State<Homescreen> with SingleTickerProviderStateMixin{
-  late TabController _controller;
-  @override
-  void initState() {
-    super.initState();
-    _controller = TabController(length: 4, vsync: this, initialIndex: 0);
-  }
+    late TabController _controller; //assign later
+    @override
+    void initState() { //runs once when widget is created
+        super.initState();
+        _controller = TabController(length: 4, vsync: this, initialIndex: 1);
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("WhatsApp Clone"),
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
-        ],
-        bottom: TabBar(
-            controller: _controller,
-            tabs: [
-                Tab(
-                    icon: Icon(Icons.camera_alt),
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text("WhatsApp Clone"),
+                actions: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                ],
+                bottom: TabBar(
+                    controller: _controller,
+                    tabs: [
+                        Tab(icon: Icon(Icons.camera_alt)),
+                        Tab(text: "CHATS"),
+                        Tab(text: "STATUS"),
+                        Tab(text: "CALLS"),                                
+                    ]
                 ),
-                Tab(
-                    text: "CHATS",
-                ),
-                Tab(
-                    text: "STATUS",
-                ),
-                Tab(
-                    text: "CALLS",
-                ),                                
-            ]),
-      ),
-    );
-  }
+            ),
+            body:TabBarView(
+                controller: _controller,
+                children: [
+                    Text("Camera"),
+                    Text("Chats"),
+                    Text("Status"),
+                    Text("Calls"),
+                ],
+            ),  
+        );
+    }
 }
