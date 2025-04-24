f135dbc5e9599d3ad33cadd4847a11c8f4be0048
romelt777 <romel.tan777@gmail.com>
2025-04-24 17:07:06 -0300

    hard coded 1 chatcard

---
+++ lib/customui/custom_card.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 30,
+      ),
+      title: Text(
+        "Rom", 
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      subtitle: Row(
+        children: [
+          Icon(Icons.done_all),
+          SizedBox(width: 3),
+          Text(
+            "Hi Rom",
+            style: TextStyle(
+              fontSize: 13
+            ),
+          )
+        ],
+      ),
+      trailing: Text("18:04"),
+
+    );
+  }
+}
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/customui/custom_card.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -11,6 +12,11 @@ class _ChatPageState extends State<ChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: ListView(
+        children: [
+          CustomCard(),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {}, 
         child: Icon(Icons.chat)
