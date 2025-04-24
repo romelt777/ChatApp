01984896b17bdb1259fbaa5ed5f71ceddabce1c5
romelt777 <romel.tan777@gmail.com>
2025-04-24 16:43:36 -0300

    adding chat page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/pages/chat_page.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,37 +18,40 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     _controller = TabController(length: 4, vsync: this, initialIndex: 1);
   }
 
+  // @override
+  // void dispose() {
+  //   _controller.dispose();
+  //   super.dispose();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("WhatsApp Clone"),
         actions: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-        PopupMenuButton<String>(
-          onSelected: (value) =>{
-            print(value)
-          },
-          itemBuilder: (BuildContext context) {
-            return [
-              PopupMenuItem(value: "New Group",child: Text("New Group")),
-              PopupMenuItem(value: "New Broadcast",child: Text("New Broadcast")),
-              PopupMenuItem(value: "WhatsApp Web",child: Text("WhatsApp Web")),
-              PopupMenuItem(value: "Starred Message",child: Text("Starred Message")),
-              PopupMenuItem(value: "Settings",child: Text("Settings")),
-            ];
-          },
-        ),
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          PopupMenuButton<String>(
+            onSelected: (value) => {print(value)},
+            itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(value: "New Group", child: Text("New Group")),
+                PopupMenuItem(value: "New Broadcast", child: Text("New Broadcast")),
+                PopupMenuItem(value: "WhatsApp Web", child: Text("WhatsApp Web")),
+                PopupMenuItem(value: "Starred Message", child: Text("Starred Message")),
+                PopupMenuItem(value: "Settings", child: Text("Settings")),
+              ];
+            },
+          ),
         ],
         bottom: TabBar(
-        controller: _controller,
-        tabs: [Tab(icon: Icon(Icons.camera_alt)), Tab(text: "CHATS"), Tab(text: "STATUS"), Tab(text: "CALLS")],
+          indicatorColor: Colors.white,
+          indicatorSize: TabBarIndicatorSize.tab,
+          controller: _controller,
+          tabs: [Tab(icon: Icon(Icons.camera_alt)), Tab(text: "CHATS"), Tab(text: "STATUS"), Tab(text: "CALLS")],
         ),
       ),
-      body: TabBarView(
-        controller: _controller,
-        children: [Text("Camera"), Text("Chats"), Text("Status"), Text("Calls")],
-      ),
+      body: TabBarView(controller: _controller, children: [Text("Camera"), ChatPage(), Text("Status"), Text("Calls")]),
     );
   }
 }
