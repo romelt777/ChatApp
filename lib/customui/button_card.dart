f52901bfef30fc74f9e60cb40aa593d3c1bb9275
romelt777 <romel.tan777@gmail.com>
2025-04-28 11:50:23 -0300

    removed imports

--- lib/customui/button_card.dart
+++ lib/customui/button_card.dart
@@ -1,6 +1,4 @@
-import 'package:chat_app/model/chat_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class ButtonCard extends StatelessWidget {
   final String name;
