b5b16aabd876b2c546cd2d418666c0f8c6b3e8dc
romelt777 <romel.tan777@gmail.com>
2026-03-07 13:58:20 -0400

    landing screen, button does not click yet

--- lib/new_screen/landing_screen.dart
+++ lib/new_screen/landing_screen.dart
@@ -22,6 +22,56 @@ class LandingScreen extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                 ),
               ),
+              SizedBox(height: MediaQuery.of(context).size.height / 8),
+              Image.asset(
+                "assets/bg.png",
+                color: Colors.greenAccent[700],
+                height: 340,
+                width: 340,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height / 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    style: TextStyle(color: Colors.black, fontSize: 17),
+                    children: [
+                      TextSpan(
+                        text: "Agree and Continue to accept the ",
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                      TextSpan(
+                        text: "WhatsApp Terms of Service and Privacy Policy",
+                        style: TextStyle(color: Colors.cyan),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              InkWell(
+                onTap: () {},
+                child: SizedBox(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width - 110,
+                  child: Card(
+                    margin: EdgeInsets.all(0),
+                    elevation: 8,
+                    color: Colors.greenAccent[700],
+                    child: Center(
+                      child: Text(
+                        "AGREE AND CONTINUE",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
