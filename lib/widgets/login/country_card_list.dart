52077564f9cf28c0a41809baa9f8e5f7e4bbf4ba
romelt777 <romel.tan777@gmail.com>
2026-03-07 15:57:45 -0400

    adding new number widget, reverting country_card_list back to stateless

--- lib/widgets/login/country_card_list.dart
+++ lib/widgets/login/country_card_list.dart
@@ -1,22 +1,17 @@
 import 'package:chat_app/model/country_model.dart';
 import 'package:flutter/material.dart';
 
-class CountryCardList extends StatefulWidget {
+class CountryCardList extends StatelessWidget {
   final CountryModel country;
   final Function setCountryData;
 
   const CountryCardList({super.key, required this.country, required this.setCountryData});
 
   @override
-  State<CountryCardList> createState() => _CountryCardListState();
-}
-
-class _CountryCardListState extends State<CountryCardList> {
-  @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        widget.setCountryData(widget.country);
+        setCountryData(country);
       },
       child: Card(
         margin: EdgeInsets.all(0.15),
@@ -25,17 +20,17 @@ class _CountryCardListState extends State<CountryCardList> {
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
           child: Row(
             children: [
-              Text(widget.country.flag),
+              Text(country.flag),
               SizedBox(
                 width: 15,
               ),
-              Text(widget.country.name),
+              Text(country.name),
               Expanded(
                 child: SizedBox(
                   width: 150,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
-                    children: [Text(widget.country.code)],
+                    children: [Text(country.code)],
                   ),
                 ),
               ),
