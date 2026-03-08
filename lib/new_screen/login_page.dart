9871f3c07d360a982038c77e8dc85987b54dc189
romelt777 <romel.tan777@gmail.com>
2026-03-08 12:54:59 -0300

    using gesture detector to fold keyboard

--- lib/new_screen/login_page.dart
+++ lib/new_screen/login_page.dart
@@ -28,71 +28,74 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //APPBAR
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Text(
-          "Enter your phone number",
-          style: TextStyle(
-            color: Colors.teal,
-            fontWeight: FontWeight.w700,
-            fontSize: 22,
-            wordSpacing: 1,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        //APPBAR
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            "Enter your phone number",
+            style: TextStyle(
+              color: Colors.teal,
+              fontWeight: FontWeight.w700,
+              fontSize: 22,
+              wordSpacing: 1,
+            ),
           ),
+          centerTitle: true,
+          actions: [
+            Icon(Icons.more_vert, color: Colors.black),
+          ],
         ),
-        centerTitle: true,
-        actions: [
-          Icon(Icons.more_vert, color: Colors.black),
-        ],
-      ),
 
-      //BODY
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Text(
-              "WhatsApp will send an SMS message to verify your number",
-              style: TextStyle(fontSize: 14.5),
-            ),
-            SizedBox(height: 5),
-            Text("What's my number?", style: TextStyle(fontSize: 12.5, color: Colors.cyan[800])),
-            SizedBox(height: 15),
-            CountrySelector(
-              country: country,
-              setCountryData: setCountryData,
-            ),
-            SizedBox(height: 15),
-            NumberWidget(
-              country: country,
-              controller: _controller,
-            ),
-            Expanded(child: Container()),
-            InkWell(
-              onTap: () {
-                if (_controller.text.isEmpty) {
-                  noPhoneNumber(context, country, _controller);
-                } else {
-                  EditPhoneNumber(context, country, _controller);
-                }
-              },
-              child: Container(
-                color: Colors.tealAccent,
-                height: 40,
-                width: 70,
-                child: Center(
-                  child: Text(
-                    "NEXT",
-                    style: TextStyle(fontWeight: FontWeight.w600),
+        //BODY
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Text(
+                "WhatsApp will send an SMS message to verify your number",
+                style: TextStyle(fontSize: 14.5),
+              ),
+              SizedBox(height: 5),
+              Text("What's my number?", style: TextStyle(fontSize: 12.5, color: Colors.cyan[800])),
+              SizedBox(height: 15),
+              CountrySelector(
+                country: country,
+                setCountryData: setCountryData,
+              ),
+              SizedBox(height: 15),
+              NumberWidget(
+                country: country,
+                controller: _controller,
+              ),
+              Expanded(child: Container()),
+              InkWell(
+                onTap: () {
+                  if (_controller.text.isEmpty) {
+                    noPhoneNumber(context, country, _controller);
+                  } else {
+                    EditPhoneNumber(context, country, _controller);
+                  }
+                },
+                child: Container(
+                  color: Colors.tealAccent,
+                  height: 40,
+                  width: 70,
+                  child: Center(
+                    child: Text(
+                      "NEXT",
+                      style: TextStyle(fontWeight: FontWeight.w600),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 60),
-          ],
+              SizedBox(height: 60),
+            ],
+          ),
         ),
       ),
     );
