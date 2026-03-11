d4e2c1ac4672341b441f57cfbc9da24635437262
romelt777 <romel.tan777@gmail.com>
2026-03-11 16:04:46 -0300

    adding new call screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:chat_app/data/chat_no_groups_data.dart';
 import 'package:chat_app/pages/camera_page.dart';
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/status_page.dart';
+import 'package:chat_app/screens/calls/call_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -53,10 +54,23 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
           indicatorColor: Colors.white,
           indicatorSize: TabBarIndicatorSize.tab,
           controller: _controller,
-          tabs: [Tab(icon: Icon(Icons.camera_alt)), Tab(text: "CHATS"), Tab(text: "STATUS"), Tab(text: "CALLS")],
+          tabs: [
+            Tab(icon: Icon(Icons.camera_alt)),
+            Tab(text: "CHATS"),
+            Tab(text: "STATUS"),
+            Tab(text: "CALLS"),
+          ],
         ),
       ),
-      body: TabBarView(controller: _controller, children: [CameraPage(), ChatPage(), StatusPage(), Text("Calls")]),
+      body: TabBarView(
+        controller: _controller,
+        children: [
+          CameraPage(),
+          ChatPage(),
+          StatusPage(),
+          CallScreen(),
+        ],
+      ),
     );
   }
 }
